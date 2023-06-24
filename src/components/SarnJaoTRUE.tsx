bb6a2c9c84c8e621b5bbd38559e34a620e48e1b9
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2023-06-24 12:02:48 +0700

    fix: something

--- src/components/SarnJaoTRUE.tsx
+++ src/components/SarnJaoTRUE.tsx
@@ -1,7 +1,11 @@
 export const SarnJaoTRUE = () => {
   return (
     <div className="w-full min-h-screen h-screen">
-      <div className="w-full h-full bg-red-600"></div>
+      <div className="w-full h-full bg-blue-100">
+        <div className="flex pt-4 text-center justify-center">
+          <h1 className="text-2xl font-bold">ศาล Jao (มั้ง) 666-</h1>
+        </div>
+      </div>
     </div>
   );
 };
